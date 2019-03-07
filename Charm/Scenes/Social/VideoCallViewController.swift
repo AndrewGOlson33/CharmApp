549a79c01ed8687679497d01440b1365e8103aa1
Daniel Pratt <daniel@blaumagier.com>
2019-03-07 16:01:47 -0500

    feat: replaced connection code
    - video call vc no longer establishes server connection
    - was able to manually join session to view call

--- Charm/Scenes/Social/VideoCallViewController.swift
+++ Charm/Scenes/Social/VideoCallViewController.swift
@@ -11,176 +11,185 @@ import OpenTok
 
 class VideoCallViewController: UIViewController {
     
-    // MARK: - IBOutlets
-    
     // MARK: - Properties
     
     // User to establish connection with
     var friend: Friend! = nil
     
     // Session Variables
-    var session: OTSession?
-    var publisher: OTPublisher?
+    lazy var session: OTSession = {
+        return OTSession(apiKey: kApiKey, sessionId: kSessionId, delegate: self)!
+    }()
+    
+    lazy var publisher: OTPublisher = {
+        let settings = OTPublisherSettings()
+        settings.name = UIDevice.current.name
+        return OTPublisher(delegate: self, settings: settings)!
+    }()
+    
     var subscriber: OTSubscriber?
     
     // OpenTok API key
     var kApiKey = TokBox.ApiKey
     // Generated session ID
-    var kSessionId = ""
+    var kSessionId = "1_MX40NjIzMjg2Mn5-MTU1MTk5MTA3NzUwMX5JM2VyOEN3QUFIeklRUUVIbFRSeWhVNVR-fg"
     // Generated token
-    var kToken = ""
+    var kToken = "T1==cGFydG5lcl9pZD00NjIzMjg2MiZzaWc9ZWNlODY4MWJhYzFjYjY4MjdkY2Y0NjM2YzAyOWU3N2I1ZjY3MTY3MjpzZXNzaW9uX2lkPTFfTVg0ME5qSXpNamcyTW41LU1UVTFNVGs1TVRBM056VXdNWDVKTTJWeU9FTjNRVUZJZWtsUlVVVkliRlJTZVdoVk5WUi1mZyZjcmVhdGVfdGltZT0xNTUxOTkxMDc4Jm5vbmNlPTAuODIwNTU1MDc3OTQwNDM4NyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTUyMDc3NDc4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"
+    
+    // Picture in Picture width / height
+    let kWidgetHeight = 240
+    let kWidgetWidth = 320
     
     // MARK: - View Lifecycle Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let configuration = URLSessionConfiguration.default
-        let session = URLSession(configuration: configuration)
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        guard let id = appDelegate.user.id else { return }
-        let room = ("\(id)+\(friend.id!)")
-        let url = URL(string: "https://charmcharismaanalytics.herokuapp.com/room/\(room)")
-        let dataTask = session.dataTask(with: url!) {
-            (data: Data?, response: URLResponse?, error: Error?) in
-            
-            guard error == nil, let data = data else {
-                print("~>Got an error: \(error!)")
-                return
-            }
-            
-            print(data)
-            
-            let dict = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [AnyHashable: Any]
-            self.kApiKey = dict?["apiKey"] as? String ?? ""
-            self.kSessionId = dict?["sessionId"] as? String ?? ""
-            self.kToken = dict?["token"] as? String ?? ""
-            print("~>Got an apiKey: \(self.kApiKey)")
-            print("~>Got a sessionid: \(self.kSessionId)")
-            print("~>Got a token: \(self.kToken)")
-            self.connectToAnOpenTokSession()
-        }
-        dataTask.resume()
-        session.finishTasksAndInvalidate()
+        doConnect()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         endCallButtonTapped(self)
     }
+    
+    // MARK: - Private Helper Functions
+    
+    /**
+     * Asynchronously begins the session connect process. Some time later, we will
+     * expect a delegate method to call us back with the results of this action.
+     */
+    fileprivate func doConnect() {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        
+        session.connect(withToken: kToken, error: &error)
+    }
+    
+    /**
+     * Sets up an instance of OTPublisher to use with this session. OTPubilsher
+     * binds to the device camera and microphone, and will provide A/V streams
+     * to the OpenTok session.
+     */
+    fileprivate func doPublish() {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        
+        session.publish(publisher, error: &error)
+        
+        if let pubView = publisher.view {
+            pubView.frame = CGRect(x: 0, y: 0, width: kWidgetWidth, height: kWidgetHeight)
+            view.addSubview(pubView)
+        }
+    }
+    
+    /**
+     * Instantiates a subscriber for the given stream and asynchronously begins the
+     * process to begin receiving A/V content for this stream. Unlike doPublish,
+     * this method does not add the subscriber to the view hierarchy. Instead, we
+     * add the subscriber only after it has connected and begins receiving data.
+     */
+    fileprivate func doSubscribe(_ stream: OTStream) {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        subscriber = OTSubscriber(stream: stream, delegate: self)
+        
+        session.subscribe(subscriber!, error: &error)
+    }
+    
+    fileprivate func cleanupSubscriber() {
+        subscriber?.view?.removeFromSuperview()
+        subscriber = nil
+    }
+    
+    fileprivate func cleanupPublisher() {
+        publisher.view?.removeFromSuperview()
+    }
+    
+    fileprivate func processError(_ error: OTError?) {
+        if let err = error {
+            DispatchQueue.main.async {
+                let controller = UIAlertController(title: "Error", message: err.localizedDescription, preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(controller, animated: true, completion: nil)
+            }
+        }
+    }
 
     // MARK: - Button Handling
     
     @IBAction func endCallButtonTapped(_ sender: Any) {
-        var error: OTError? = nil
-        session?.disconnect(&error)
-        if let error = error {
-            print("~>There was an error: \(error)")
-            if let _ = sender as? UIButton {
-                navigationController?.popViewController(animated: true)
-            }
-        } else {
-            print("~>Successfully disconnected.")
-        }
-        
-        
     }
     
-    func connectToAnOpenTokSession() {
-        session = OTSession(apiKey: kApiKey, sessionId: kSessionId, delegate: self)
-        var error: OTError?
-        session?.connect(withToken: kToken, error: &error)
-        if error != nil {
-            print(error!)
-        }
-    }
-
 }
 
-// MARK: - OTSessionDelegate callbacks
+// MARK: - OTSession delegate callbacks
 extension VideoCallViewController: OTSessionDelegate {
-    
     func sessionDidConnect(_ session: OTSession) {
-        print("~>The client connected to the OpenTok session.")
-        let settings = OTPublisherSettings()
-        settings.name = UIDevice.current.name
-        
-        guard let publisher = OTPublisher(delegate: self as? OTPublisherKitDelegate, settings: settings) else {
-            return
-        }
-        
-        var error: OTError?
-        session.publish(publisher, error: &error)
-        guard error == nil else {
-            print(error!)
-            return
-        }
-        
-        guard let publisherView = publisher.view else {
-            return
-        }
-        
-        publisher.publishVideo = true
-        publisher.publishAudio = true
-        
-        view.addSubview(publisherView)
-        view.sendSubviewToBack(publisherView)
-        
-        publisherView.translatesAutoresizingMaskIntoConstraints = false
-        let horizontalConstraint = NSLayoutConstraint(item: publisherView, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)
-        let verticalConstraint = NSLayoutConstraint(item: publisherView, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 0)
-        let widthConstraint = NSLayoutConstraint(item: publisherView, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: view.frame.width)
-        let heightConstraint = NSLayoutConstraint(item: publisherView, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: view.frame.height)
-         view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        print("~>Session connected")
+        doPublish()
     }
     
     func sessionDidDisconnect(_ session: OTSession) {
-        print("The client disconnected from the OpenTok session.")
+        print("~>Session disconnected")
     }
     
-    func session(_ session: OTSession, didFailWithError error: OTError) {
-        print("The client failed to connect to the OpenTok session: \(error).")
-    }
     func session(_ session: OTSession, streamCreated stream: OTStream) {
-        print("~>A stream was created in the session.")
-        subscriber = OTSubscriber(stream: stream, delegate: self)
-        guard let subscriber = subscriber else {
-            return
+        print("~>Session streamCreated: \(stream.streamId)")
+        if subscriber == nil {
+            doSubscribe(stream)
         }
-        
-        var error: OTError?
-        session.subscribe(subscriber, error: &error)
-        guard error == nil else {
-            print("~>Error subscribing to session: \(String(describing: error))")
-            return
-        }
-        
-        guard let subscriberView = subscriber.view else {
-            return
-        }
-        
-        subscriberView.frame = UIScreen.main.bounds
-        view.insertSubview(subscriberView, at: 0)
     }
     
     func session(_ session: OTSession, streamDestroyed stream: OTStream) {
-        print("A stream was destroyed in the session.")
+        print("~>Session streamDestroyed: \(stream.streamId)")
+        if let subStream = subscriber?.stream, subStream.streamId == stream.streamId {
+            cleanupSubscriber()
+        }
+    }
+    
+    func session(_ session: OTSession, didFailWithError error: OTError) {
+        print("~>session Failed to connect: \(error.localizedDescription)")
+    }
+    
+}
+
+// MARK: - OTPublisher delegate callbacks
+extension VideoCallViewController: OTPublisherDelegate {
+    func publisher(_ publisher: OTPublisherKit, streamCreated stream: OTStream) {
+        print("~>Publishing")
+    }
+    
+    func publisher(_ publisher: OTPublisherKit, streamDestroyed stream: OTStream) {
+        cleanupPublisher()
+        if let subStream = subscriber?.stream, subStream.streamId == stream.streamId {
+            cleanupSubscriber()
+        }
+    }
+    
+    func publisher(_ publisher: OTPublisherKit, didFailWithError error: OTError) {
+        print("~>Publisher failed: \(error.localizedDescription)")
     }
 }
 
-extension VideoCallViewController: OTSubscriberKitDelegate {
-    
-    public func subscriberDidConnect(toStream subscriber: OTSubscriberKit) {
-        print("~>The subscriber did connect to the stream.")
+// MARK: - OTSubscriber delegate callbacks
+extension VideoCallViewController: OTSubscriberDelegate {
+    func subscriberDidConnect(toStream subscriberKit: OTSubscriberKit) {
+        print("~>Subscriber did connect, setting up view.")
+        if let subsView = subscriber?.view {
+            subsView.frame = CGRect(x: 0, y: kWidgetHeight, width: kWidgetWidth, height: kWidgetHeight)
+            view.addSubview(subsView)
+            view.bringSubviewToFront(subsView)
+        }
     }
     
-    public func subscriber(_ subscriber: OTSubscriberKit, didFailWithError error: OTError) {
-        print("~>The subscriber failed to connect to the stream.")
+    func subscriber(_ subscriber: OTSubscriberKit, didFailWithError error: OTError) {
+        print("~>Subscriber failed: \(error.localizedDescription)")
     }
-    
-    func subscriberDidDisconnect(fromStream subscriber: OTSubscriberKit) {
-        print("~>The subscriber did disconnect from the stream.")
-    }
-    
 }
