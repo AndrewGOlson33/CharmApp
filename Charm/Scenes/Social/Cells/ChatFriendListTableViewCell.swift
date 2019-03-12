6373630613d73c85d8305c88da7a85b6c36e8aa9
Daniel Pratt <daniel@blaumagier.com>
2019-03-12 15:31:10 -0400

    feat: enabled profile images on chat friend list page
    - created a custom cell object for chat friend list
    - configured storyboard
    - setup cell so that the profile picture will look circular

---
+++ Charm/Scenes/Social/Cells/ChatFriendListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatFriendListTableViewCell.swift
+//  Charm
+//
+//  Created by Daniel Pratt on 3/12/19.
+//  Copyright © 2019 Charm, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ChatFriendListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
